238b9542e64322b45f84425c478b3237313e9908
Francis <fraanciisq@gmail.com>
2024-01-27 00:07:35 +0800

    desc-resp

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,6 +1,10 @@
+// page.js
+
 'use client';
-import styles from './page.module.scss'
-import { useEffect, useState } from 'react'
+
+import Head from 'next/head';
+import styles from './page.module.scss';
+import { useEffect, useState } from 'react';
 import { AnimatePresence } from 'framer-motion';
 import Preloader from '../components/Preloader';
 import Landing from '../components/Landing';
@@ -10,34 +14,37 @@ import SlidingImages from '../components/SlidingImages';
 import Contact from '../components/Contact';
 
 export default function Home() {
-
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect( () => {
-    (
-      async () => {
-          const LocomotiveScroll = (await import('locomotive-scroll')).default
-          const locomotiveScroll = new LocomotiveScroll();
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import('locomotive-scroll')).default;
+      const locomotiveScroll = new LocomotiveScroll();
 
-          setTimeout( () => {
-            setIsLoading(false);
-            document.body.style.cursor = 'default'
-            window.scrollTo(0,0);
-          }, 2000)
-      }
-    )()
-  }, [])
+      setTimeout(() => {
+        setIsLoading(false);
+        document.body.style.cursor = 'default';
+        window.scrollTo(0, 0);
+      }, 2000);
+    })();
+  }, []);
 
   return (
-    <main className={styles.main}>
-      <AnimatePresence mode='wait'>
-        {isLoading && <Preloader />}
-      </AnimatePresence>
-      <Landing />
-      <Description />
-      <Projects />
-      <SlidingImages />
-      <Contact />
-    </main>
-  )
+    <>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </Head>
+      <main className={styles.main}>
+        <AnimatePresence mode="wait">
+          {isLoading && <Preloader />}
+        </AnimatePresence>
+        <Landing />
+        <Description />
+        <Projects />
+        <SlidingImages />
+        <Contact />
+      </main>
+    </>
+  );
 }
