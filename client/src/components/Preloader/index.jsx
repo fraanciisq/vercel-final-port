2e39a4af3c9fc175ea521753ab3b4f1802b628e7
Francis <fraanciisq@gmail.com>
2024-01-31 20:44:32 +0800

    nav

--- client/src/components/Preloader/index.jsx
+++ client/src/components/Preloader/index.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import { opacity, slideUp } from './anim';
 
-const words = ["Hello", "Bonjour", "Ciao", "Olà", "やあ", "Hallå", "Guten tag", "Hallo"]
+const words = ["Hello", "Bonjour", "Ciao", "Olà", "やあ", "Hallå", "Guten tag", "Kumusta"]
 
 export default function Index() {
     const [index, setIndex] = useState(0);
