a90fa2e0cf3200287e6a87a38008a780461b493f
Francis <fraanciisq@gmail.com>
2024-01-31 21:52:04 +0800

    foot

--- client/src/components/Header/nav/Footer/index.jsx
+++ client/src/components/Header/nav/Footer/index.jsx
@@ -3,7 +3,7 @@ import styles from './style.module.scss';
 export default function index() {
   return (
     <div className={styles.footer}>
-        <a>Awwwards</a>
+        <a>X or Twitter</a>
         <a>Instagram</a>
         <a>Dribble</a>
         <a>LinkedIn</a>
