ccb0426c5b33ed9a777c74c6c719aac61bb85902
Francis <fraanciisq@gmail.com>
2024-02-04 21:35:41 +0800

    project-link

--- client/src/components/Projects/components/project/index.jsx
+++ client/src/components/Projects/components/project/index.jsx
@@ -2,36 +2,49 @@ import React from 'react';
 import styles from './style.module.scss';
 
 export default function Index({ index, title, manageModal }) {
-  const handleMouseEnter = (e) => {
-    manageModal(true, index, e.clientX, e.clientY);
-  };
+    const handleMouseEnter = (e) => {
+        manageModal(true, index, e.clientX, e.clientY);
+    };
 
-  const handleMouseLeave = (e) => {
-    manageModal(false, index, e.clientX, e.clientY);
-  };
+    const handleMouseLeave = (e) => {
+        manageModal(false, index, e.clientX, e.clientY);
+    };
 
-  const handleTouchStart = (e) => {
-    e.preventDefault();
-    const touch = e.touches[0];
-    manageModal(true, index, touch.clientX, touch.clientY);
-  };
+    const handleTouchStart = (e) => {
+        e.preventDefault();
+        const touch = e.touches[0];
+        manageModal(true, index, touch.clientX, touch.clientY);
+    };
 
-  const handleTouchEnd = (e) => {
-    e.preventDefault();
-    const touch = e.changedTouches[0];
-    manageModal(false, index, touch.clientX, touch.clientY);
-  };
+    const handleTouchEnd = (e) => {
+        e.preventDefault();
+        const touch = e.changedTouches[0];
+        manageModal(false, index, touch.clientX, touch.clientY);
+    };
 
-  return (
-    <div
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      onTouchStart={handleTouchStart}
-      onTouchEnd={handleTouchEnd}
-      className={styles.project}
-    >
-      <h2>{title}</h2>
-      <p>Software and App Development</p>
-    </div>
-  );
+    const handleProjectClick = () => {
+        // Check if the clicked project is "TOPSystem" or "Eco Pro WP" before redirecting
+        if (title === 'TOPSystem - Oracle') {
+            // Replace 'your-link-here' with the actual link for "TOPSystem"
+            const projectLink = 'https://tops.net.ph';
+            window.location.href = projectLink;
+        } else if (title === 'Eco Pro WP') {
+            // Redirect to www.ecoprodisinfectpm.com for "Eco Pro WP"
+            window.location.href = 'https://www.ecoprodisinfectpm.com';
+        }
+    };
+
+    return (
+        <div
+            onClick={() => handleProjectClick()}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            onTouchStart={handleTouchStart}
+            onTouchEnd={handleTouchEnd}
+            className={styles.project}
+        >
+            <h2>{title}</h2>
+            <p>Software and App Development</p>
+        </div>
+    );
 }
